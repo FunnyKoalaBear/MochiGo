28d530a012d004d5555b1906a41d2f27326d1cfe
FunnyKoalaBear <sbmithun@gmail.com>
2026-02-21 21:21:23 +0900

    Omni Microphone initial commit

---
+++ rsp-scripts/omniMic.py
@@ -0,0 +1,23 @@
+#This program will be used to program the functionality of the INMP441  omnidirection microphone module 
+
+
+
+#mic functionality
+class Microphone():
+
+    def __init__(self):
+        pass
+    
+    def record(self):
+        pass
+
+    def play(self):
+        pass
+
+
+#runs this if file is run directly 
+if __name__ == "__main__":
+    mic = Microphone()
+
+    while True:
+        pass
