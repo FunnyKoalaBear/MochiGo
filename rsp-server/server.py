d42295a96c4e9aafbd218d24491ef0f3ee5e8eef
FunnyKoalaBear <sbmithun@gmail.com>
2026-03-10 17:43:08 +0900

    Cloud server and local server intiial communication successful

--- rsp-server/server.py
+++ rsp-server/server.py
@@ -4,6 +4,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import uvicorn #used to start the server 
 import subprocess
+import time
 
 #importing classes 
 from ai_pipeline import pipeline, close
@@ -11,17 +12,32 @@ from ai_pipeline import pipeline, close
 #staring server
 app = FastAPI()
 
-#run this line in a seperate terminal to make server public 
-#tailscale serve http://127.0.0.1:8000
-subprocess.run("tailscale down")
-subprocess.run("tailscale up")
-subprocess.run("tailscale --bg serve http://127.0.0.1:8000")
-subprocess.run("tailscale status")
+def setup_tailscale():
+    #run this line in a seperate terminal to make server public 
+    #tailscale serve http://127.0.0.1:8000
+    subprocess.run("tailscale down")
+    subprocess.run("tailscale up")
+    subprocess.run("tailscale --bg serve http://127.0.0.1:8000")
+    subprocess.run("tailscale status")
+
+
+#switchboard to communicate between the 2 websocket connections 
+class Switchboard():
+
+    def __init__(self, ws):
+        self.ws = ws
+
+    def switch():
+        pass
+
 
 #end point, route decorator 
+#function that manages connection between mochigo client and local server 
 @app.websocket("/ws/mochi")
 async def websocket_endpoint(websocket: WebSocket):
+    
     await websocket.accept()
+    print("Connection made at /ws/mochi")
 
     try:
         while True:
@@ -31,40 +47,67 @@ async def websocket_endpoint(websocket: WebSocket):
             #print(f"Text receieved was {data}")  
 
             #run ai pipeline
-            audioOut = await pipeline(data)
-            print(audioOut)
+            llmOut = await pipeline(data)
+            print(llmOut)
 
-            #sending data back 
-            await websocket.send_text(audioOut)
+
+            #use switchboard to send data to google colab computer for tts 
+
+
+            #recieve speech data from google colab from switchboard 
+
+
+            #send speech data to mochi
+            #need to change llmOut to ttsOut soon 
+            await websocket.send_text(llmOut)
     
             #loop back 
 
-    except WebSocketDisconnect or KeyboardInterrupt:
+    except (WebSocketDisconnect, KeyboardInterrupt):
         print("Server closing.")
         subprocess.run("tailscale down")
         close()
+        exit()
 
+
+
+#function that manages websocket connection between google colab cloud computer
 @app.websocket("/ws/tts")
 async def ttsAudio(websocket: WebSocket):
+    
     await websocket.accept()
+    print("Connection made at /ws/tts")
+    time.sleep(1)
 
     try:
         while True:
-            #sending text data to colab computer 
-            pass
+
+            #sending llm output to colab server
+            print("Going to send")
+            llmOut = "Thats nice to hear, I am glad you had a great day today" 
+            await websocket.send_text(llmOut)
 
             #recieving tts audio data from colab computer 
+            dataOut = await websocket.receive_text()
+            print(f"Speech data recieved was: {dataOut}")
+            
+            #switchboard call to send data back to main function
+
+            #loopback
+            time.sleep(1)
 
 
-            #sending audio back to ai_pipeline 
-    
-    except:
-        pass
+    except (WebSocketDisconnect, KeyboardInterrupt):
+        print("Server closing.")
+        subprocess.run("tailscale down")
+        close()
+        exit()
 
 
 
 if __name__ == "__main__":
     #starting server
+    setup_tailscale()
     uvicorn.run("server:app", host="0.0.0.0", port=8000)
 
 
